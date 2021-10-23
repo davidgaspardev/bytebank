755232504c3f276200bdd630d7e11589e1fac9f8
David Gaspar <davidgaspar.dev@gmail.com>
2021-10-23 12:14:06 -0300

    Transfer data model created

---
+++ lib/src/helpers/abstractions/model.dart
@@ -0,0 +1,14 @@
+import 'dart:convert';
+
+/// Abstraction for models
+abstract class Model {
+
+  const Model();
+
+  Map<String, dynamic> toMap();
+
+  @override
+  String toString() {
+    return jsonEncode(toMap());
+  }
+}
---
+++ lib/src/models/transfer_data.dart
@@ -0,0 +1,21 @@
+import 'package:bytebank/src/helpers/abstractions/model.dart';
+
+/// Transfer data model
+class TransferData extends Model {
+
+  final int accountNumber;
+  final double value;
+
+  const TransferData({
+    required this.accountNumber,
+    required this.value
+  });
+
+  @override
+  Map<String, dynamic> toMap() {
+    return {
+      "accountNumber": accountNumber,
+      "value": accountNumber
+    };
+  }
+}
