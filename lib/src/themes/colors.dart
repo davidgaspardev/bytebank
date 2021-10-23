479c017b9017c20b14f0b9d7a2d8b0a0d9871bad
David Gaspar <davidgaspar.dev@gmail.com>
2021-10-23 10:17:38 -0300

    Theme colors created

---
+++ lib/src/themes/colors.dart
@@ -0,0 +1,16 @@
+import 'package:flutter/material.dart';
+
+class ThemeColors {
+  static const theme = <int, Color>{
+    50: Color(0xFF404040),
+    100: Color(0xFF3D3D3D),
+    200: Color(0xFF393939),
+    300: Color(0xFF353535),
+    400: Color(0xFF313131),
+    500: Color(0xFF2D2D2D),
+    600: Color(0xFF292929),
+    700: Color(0xFF252525),
+    800: Color(0xFF212121),
+    900: Color(0xFF1D1D1D)
+  };
+}
