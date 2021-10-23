0cbece378b21663beb142029b83d2bda6e85b296
David Gaspar <davidgaspar.dev@gmail.com>
2021-10-23 15:14:09 -0300

    Transfer item widget created

---
+++ lib/src/screens/home/widgets/transfer_item.dart
@@ -0,0 +1,23 @@
+import 'package:bytebank/src/models/transfer_data.dart';
+import 'package:flutter/material.dart';
+
+class TransferItem extends StatelessWidget {
+
+  final TransferData data;
+
+  const TransferItem({
+    Key? key,
+    required this.data
+  }): super(key: key);
+
+  @override
+  Widget build(BuildContext context) {
+    return Card(
+      child: ListTile(
+        leading: const Icon(Icons.monetization_on),
+        title: Text(data.accountNumber.toString()),
+        subtitle: Text(data.value.toString()),
+      ),
+    );
+  }
+}
