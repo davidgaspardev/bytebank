9a394509936087961d87bb10c80ba4bf770826d3
David Gaspar <davidgaspar.dev@gmail.com>
2021-10-23 10:49:55 -0300

    Simple transfer screen

---
+++ lib/src/screens/transfer/transfer.dart
@@ -0,0 +1,73 @@
+import 'package:flutter/material.dart';
+
+class TransferScreen extends StatelessWidget {
+  
+  TransferScreen({ Key? key }): super(key: key);
+
+  final accountNumberControl = TextEditingController();
+  final valueControl = TextEditingController();
+
+  @override
+  Widget build(BuildContext context) {
+    return Scaffold(
+      appBar: AppBar(
+        title: const Text("Criar transferência"),
+      ),
+      body: Container(
+        padding: const EdgeInsets.symmetric(horizontal: 16, vertical: 8),
+        child: Column(
+          children: [
+            TextField(
+              controller: accountNumberControl,
+              style: const TextStyle(
+                fontSize: 20
+              ),
+              decoration: const InputDecoration(
+                label: Text("Número da conta"),
+                hintText: "0000",
+              ),
+            ),
+            TextField(
+              controller: valueControl,
+              style: const TextStyle(
+                fontSize: 20
+              ),
+              decoration: const InputDecoration(
+                label: Text("Valor"),
+                hintText: "0.00",
+                icon: Icon(Icons.monetization_on)
+              ),
+            ),
+            ElevatedButton(
+              onPressed: () {
+                int? accountNumber = int.tryParse(accountNumberControl.text);
+                double? value = double.tryParse(valueControl.text);
+
+                if(accountNumber != null && value != null) {
+                  var transfer = Transfer(accountNumber: accountNumber, value: value);
+                  debugPrint("tranfer: $transfer");
+                }
+              },
+              child: const Text("Confirmar")
+            ),
+          ],
+        ),
+      ),
+    );
+  }
+}
+
+class Transfer {
+  final int accountNumber;
+  final double value;
+
+  const Transfer({
+    required this.accountNumber,
+    required this.value
+  });
+
+  @override
+  String toString() {
+    return "{ accountNumber: $accountNumber, value: $value }";
+  }
+}
