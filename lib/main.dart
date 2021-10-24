76c8900147bf90d2ea8cf5c5a6c34583b7479820
David Gaspar <davidgaspar.dev@gmail.com>
2021-10-23 21:19:33 -0300

    Title updated (MaterialApp)

--- lib/main.dart
+++ lib/main.dart
@@ -12,7 +12,7 @@ class ByteBank extends StatelessWidget {
   @override
   Widget build(BuildContext context) {
     return MaterialApp(
-      title: 'Flutter Demo',
+      title: 'ByteBank',
       theme: ThemeData(
         // This is the theme of your application.
         //
