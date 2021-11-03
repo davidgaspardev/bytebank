006e9177871f4437b13926c1f585d406c49359d0
David Gaspar <davidgaspar.dev@gmail.com>
2021-11-03 01:09:17 -0300

    Id field (optional) added to the Transaction entity

--- lib/src/models/transaction_data.dart
+++ lib/src/models/transaction_data.dart
@@ -2,11 +2,13 @@ import 'package:bytebank/src/helpers/abstractions/model.dart';
 import 'package:bytebank/src/models/contact_data.dart';
 
 class TransactionData extends Model {
+  final String? id;
   final double value;
   final ContactData contact;
   final DateTime dateTime;
 
   const TransactionData({
+    this.id,
     required this.value,
     required this.contact,
     required this.dateTime,
@@ -15,6 +17,7 @@ class TransactionData extends Model {
   static TransactionData fromMap(Map<String, dynamic> map) {
     try {
       return TransactionData(
+        id: map["id"],
         value: map["value"] is int ? map["value"].toDouble() : map["value"],
         contact: ContactData.fromMap(map["contact"]),
         dateTime: DateTime.parse(map["dateTime"]),
@@ -26,10 +29,14 @@ class TransactionData extends Model {
 
   @override
   Map<String, dynamic> toMap() {
-    return {
+    var map = <String, dynamic>{
       "value": value,
       "contact": contact.toMap(),
       "datetime": dateTime.toIso8601String(),
     };
+
+    if(id != null) map["id"] = id;
+
+    return map;
   }
 }
