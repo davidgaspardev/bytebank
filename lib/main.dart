9730cfdf46a4dfa5ec4c7ba004f864d06f0e7755
David Gaspar <davidgaspar.dev@gmail.com>
2021-11-07 16:34:33 -0300

    Pass uncaught errors from the Flutter to Crashlytics

--- lib/main.dart
+++ lib/main.dart
@@ -1,8 +1,18 @@
 import 'package:bytebank/src/helpers/routes.dart';
 import 'package:bytebank/src/themes/themes.dart';
+import 'package:firebase_core/firebase_core.dart';
+import 'package:firebase_crashlytics/firebase_crashlytics.dart';
 import 'package:flutter/material.dart';
 
-void main() => runApp(const ByteBank());
+void main() async {
+  WidgetsFlutterBinding.ensureInitialized();
+
+  await Firebase.initializeApp();
+  // Pass all uncaught errors from the framework to Crashlytics.
+  FlutterError.onError = FirebaseCrashlytics.instance.recordFlutterError;
+
+  runApp(const ByteBank());
+}
 
 class ByteBank extends StatelessWidget {
   const ByteBank({Key? key}) : super(key: key);
